294be21ce4ae65411241ae87ec04f0355f9482ab
Lucas Silva <lucasvida2005@gmail.com>
2025-03-31 15:05:56 -0300

    decorating dialog box

--- lib/util/dialog_box.dart
+++ lib/util/dialog_box.dart
@@ -13,6 +13,7 @@ class DialogBox extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return AlertDialog(
+      backgroundColor: Colors.white,
       title: Text("Add new task"),
       content: TextField(
         controller: textController,
@@ -20,25 +21,44 @@ class DialogBox extends StatelessWidget {
       ),
       actions: [
         Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             // Cancel button
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-                textController.clear();
-              },
-              child: Text("Cancel"),
+            Expanded(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueGrey[700],
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                  textController.clear();
+                },
+                child: Text("Cancel"),
+              ),
             ),
 
             SizedBox(width: 10),
 
             // Add button
-            ElevatedButton(
-              onPressed: () {
-                Navigator.pop(context);
-                onAdd();
-              },
-              child: Text("Add"),
+            Expanded(
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.blueGrey[700],
+                  foregroundColor: Colors.white,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.pop(context);
+                  onAdd();
+                },
+                child: Text("Add"),
+              ),
             ),
           ],
         ),
